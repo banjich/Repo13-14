e935c06f424b44e40813b781b9a60f179ceaac0d
banjich <banjich@gmail.com>
2021-09-27 10:13:59 +0200

    Added folder ex01

---
+++ ex01/scripts/index.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementsById(myModal).style.display = "block";
+}
+
+function closeModal() {
+    document.getElementsById(myModal).style.display = "none";
+}
